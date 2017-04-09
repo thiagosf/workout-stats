10686019e4c381dd76039fddb5c0903ab81e78c8
Thiago Silva Ferreira <thiagosfsjc@gmail.com>
2017-04-09 15:32:33 -0300

    Criando variavel para cor principal do app

--- src/components/pieces/DirectionActions.vue
+++ src/components/pieces/DirectionActions.vue
@@ -1,19 +1,11 @@
 <template>
   <div class="direction-actions">
-    <a
-      href="#"
-      class="prev"
-      @click="onPrev"
-      >
+    <span class="prev" @click="onPrev">
       <icon name="left-arrow" />
-    </a>
-    <a
-      href="#"
-      class="next"
-      @click="onNext"
-      >
+    </span>
+    <span class="next" @click="onNext">
       <icon name="right-arrow" />
-    </a>
+    </span>
   </div>
 </template>
 
